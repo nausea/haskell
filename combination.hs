f219f3ba42b664143fb0ff77fe99c599ce699fbc
Nausea Atg <atg.nausea@gmail.com>
2009-05-11 16:37:20 +0300

    added tail-recursive comb function

--- combination.hs
+++ combination.hs
@@ -1,2 +1,7 @@
 combinations []     = [[]]
 combinations (x:xs) = combinations xs ++ [(x:xs')| xs' <- combinations xs]
+
+-- Tail recursive version:
+comb xs = comb' xs [[]]
+    where comb' []     acc = acc
+          comb' (x:xs) acc = comb' xs (acc ++ map (x:) acc)
