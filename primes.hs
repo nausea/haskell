cab4ec21769116556289d8b1b87bb93c4f0a9a74
Nausea Atg <atg.nausea@gmail.com>
2009-05-17 21:49:52 +0300

    a little improvement about primes

--- primes.hs
+++ primes.hs
@@ -1,4 +1,4 @@
-primes = 2 : [x | x <- [3..], all (\y -> mod x y /= 0) $
+primes = 2 : [x | x <- [3,5..], all (\y -> mod x y /= 0) $
               takeWhile (\y -> y * y <= x) primes]
 
-main = putStrLn $ show $ take 10000 primes 
+main = putStrLn $ show $ take 500000 primes 
